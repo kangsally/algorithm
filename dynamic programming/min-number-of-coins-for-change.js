f8aea3239419a77a2ddad815244d9643f240da5f
kangsuroro <kangsuroro2907@gmail.com>
2021-05-10 23:10:34 +0900

    Add minNumberOfCoinsForChange

---
+++ dynamic programming/min-number-of-coins-for-change.js
@@ -0,0 +1,16 @@
+function minNumberOfCoinsForChange(n, denoms) {
+  // Write your code here.
+  const numOfCoins = new Array(n + 1).fill(Infinity);
+  numOfCoins[0] = 0;
+  for(const denom of denoms){
+    for(let amount = 0; amount < numOfCoins.length; amount++){
+      if(denom <= amount){
+        numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1);
+      }
+    }
+  }
+  return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
+}
+
+// Do not edit the line below.
+exports.minNumberOfCoinsForChange = minNumberOfCoinsForChange;
